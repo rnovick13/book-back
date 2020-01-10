cca2d04a22ee0565b5a06229d15e32840d350713
rnovick13 <rnovick13@gmail.com>
2020-01-10 10:16:31 -0500

    fix createbook

--- db/migrate/001_create_books.rb
+++ db/migrate/001_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
       t.string :title
-      t.integer :author_id
+      t.string :author
       t.string :cover
 
       t.timestamps

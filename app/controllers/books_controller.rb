851d51f71affc386b2cc132048ab73612b36e5b6
rnovick13 <rnovick13@gmail.com>
2020-01-10 10:29:51 -0500

    seeding works book controller has index and create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,21 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
+    render json: @books
+  end
+
+  def create
+    @book = Book.create(review_params)
+    render json: @review
+  end
+
+  def show
+  end
+
+  private
+    def review_params
+      params.require(:book).permit(:title, :author, :cover)
+    end
+
 end
